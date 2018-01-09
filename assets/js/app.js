84f25498fc4a795d26e6c2afa01296d677a66fe3
mthompsonc <m.paz.thompson@gmail.com>
2018-01-09 19:45:05 -0300

    extrayendo información de api y plasmándola en html por medio de jquery

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
- 
+
 });
 
 var searchBtn = $('#searchBtn');
@@ -47,3 +47,14 @@ function renderError (error) {
 function renderDetails () {
   console.log('details');
 }
+
+  $.getJSON('http://www.omdbapi.com/?t=Harry+Potter+and+the+Chamber+of+Secrets&apikey=276881c0').then(function(paste) {
+    var poster= $('#poster');
+    var datos= $('#datos');
+
+    poster.append('<img src="' + paste.Poster + '" alt="poster">');
+
+    datos.append('<h3>' + paste.Title + '</h3> <h4>Año:</h4><p>' + paste.Year + '</p><h4>Duración: </h4><p>' + paste.Runtime + '</p><h4>Director:</h4><p>' + paste.Director + '</p><h4>Escritor: </h4><p>' + paste.Writer + '</p><h4>Actores: </h4><p>' + paste.Actors + '</p><a href="' + paste.Website + '">Website Oficial</a>');
+
+
+  });

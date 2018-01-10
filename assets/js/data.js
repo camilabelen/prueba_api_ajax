387ab051385149b93fa02bd508a7c71adc304971
mthompsonc <m.paz.thompson@gmail.com>
2018-01-09 21:31:51 -0300

    agregando base de datos

---
+++ assets/js/data.js
@@ -0,0 +1,46 @@
+var data = [
+  {
+  "nombre": "Harry Potter y la piedra filosofal",
+  "ebook": "https://www.amazon.com/s/ref=nb_sb_noss_1?url=search-alias%3Daps&field-keywords=harry+potter+ebook",
+  "libro": "https://www.feriachilenadellibro.cl/catalogsearch/result/?q=harry+potter+y+la+piedra+filosofal",
+  // Poner el resultado de búsqueda de la librería para que el usuario tenga opción de elegir cuál edición desea comprar.
+  "genero": "Fantasía, Familiar",
+  "trailer": "https://www.youtube.com/watch?v=uwM2oPGQMl4" // Buscar trailer de película con subtítulos si es en inglés.
+  },
+  {
+  "nombre": "",
+  "ebook": "",
+  "libro": "",
+  // Poner el resultado de búsqueda de la librería para que el usuario tenga opción de elegir cuál edición desea comprar.
+  "genero": "Fantasía, Familiar",
+  "trailer": ""
+  // Buscar trailer de película con subtítulos si es en inglés.
+  },
+  {
+  "nombre": "",
+  "ebook": "",
+  "libro": "",
+  // Poner el resultado de búsqueda de la librería para que el usuario tenga opción de elegir cuál edición desea comprar.
+  "genero": "Fantasía, Familiar",
+  "trailer": ""
+  // Buscar trailer de película con subtítulos si es en inglés.
+  },
+  {
+  "nombre": "",
+  "ebook": "",
+  "libro": "",
+  // Poner el resultado de búsqueda de la librería para que el usuario tenga opción de elegir cuál edición desea comprar.
+  "genero": "Fantasía, Familiar",
+  "trailer": ""
+  // Buscar trailer de película con subtítulos si es en inglés.
+  },
+  {
+  "nombre": "",
+  "ebook": "",
+  "libro": "",
+  // Poner el resultado de búsqueda de la librería para que el usuario tenga opción de elegir cuál edición desea comprar.
+  "genero": "Fantasía, Familiar",
+  "trailer": ""
+  // Buscar trailer de película con subtítulos si es en inglés.
+  },
+]

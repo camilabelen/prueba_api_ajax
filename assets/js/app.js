09fdf15aa80b02a84d3147487d3e5afbb6561011
camilabelen <ccornejoss@gmail.com>
2018-01-11 08:54:19 -0300

    actualizacion js

--- assets/js/app.js
+++ assets/js/app.js
@@ -72,6 +72,13 @@ function renderMovies (response) {
                       + '</p><h4>Actores: </h4><p>' + paste.Actors 
                       + '</p><a href="' + paste.Website + '">Website Oficial</a>');
     }
+      var x = $(this).text();
+      for(var i = 0; i< x.length; i++){
+        if(x.charAt(i) == " ") {
+          var str = x.replace(" ", "+");
+        }        
+      }      
+      console.log(str);
   });
 
 
@@ -81,4 +88,40 @@ function renderError (error) {
   console.log(error);
 }
 
+/*
+function renderDetails () {
+
+  $('.result').click(function (response) {
+    var movies = response.Search;
+    var resultsUl = $('#results');
+    resultsUl.empty();
+    for (var m in movies) {
+      var movie = movies[m];
+      var title = movie.Title;
+      var imdbID = movie.imdbID;
+      var poster = movie.Poster;
+    }   
+    //sacar id del elemento al que se le haga click
+    var urlMovie = 'http://www.omdbapi.com/?i=' + imdbID + '&apikey=276881c0';
+  })  
+  console.log(urlMovie);
+}
+
+
+  $.getJSON('http://www.omdbapi.com/?t=Harry+Potter+and+the+Chamber+of+Secrets&apikey=276881c0').then(function(paste) {
+     var poster = $('#poster');
+     var titulo = $('#titulo');
+     var datos = $('#datos');
+     var rating = $('#rating');
+
+     poster.append('<img src="' + paste.Poster + '" alt="poster">');
+     titulo.append('<h4 style="font-weight: bold;">' + paste.Title + '</h4>');
+     datos.append('<h6 class="h6bigger">Año:</h6><p>' + paste.Year + '</p><h6 class="h6bigger">Duración: </h6><p>' + paste.Runtime + '</p><h6 class="h6bigger">Director:</h6><p>' + paste.Director + '</p><h6 class="h6bigger">Escritor: </h6><p>' + paste.Writer + '</p><h6 class="h6bigger">Actores: </h6><p>' + paste.Actors + '</p><a class=center-align href="' + paste.Website + '">Website Oficial</a>');
+
+     rating.append('<div style="background-color:#ffbf00; border-radius: 20px; width:8em; padding:0.3em; text-align:center; margin-top: 1em; display: inline-block;"> <h6 class="h6bigger">Rating:</h6><p style="font-size:1em;"><strong>'+ paste.imdbRating +'</strong> </p></div>');
+
+   });
+*/
+
+>>>>>>> Stashed changes
  
